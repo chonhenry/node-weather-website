7e0e4c789c06e802afc9fc55b5d24f9a4f71558b
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-06-22 00:03:39 -0400

    init commit

---
+++ src/utils/forecsat.js
@@ -0,0 +1,20 @@
+const request = require("request");
+
+const forecast = (lat, long, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=346c39a8494a4b109709fb15164e3cc2&query=${lat},${long}&units=f`;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. `
+      );
+    }
+  });
+};
+
+module.exports = forecast;
